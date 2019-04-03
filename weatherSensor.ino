a77c5beeb41966930170ceb383090a26c341d884
Luis Diego Mora <diego.morajimenez@ucr.ac.cr>
2019-04-03 16:29:50 -0600

    Add functions to display data and format code

--- weatherSensor.ino
+++ weatherSensor.ino
@@ -11,14 +11,14 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define chipSelect 4
+#define CHIP_SELECT 4
 #define BUTTON_A 9
 #define BUTTON_B 6
 #define BUTTON_C 5
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire); // SSD1306 display
-Adafruit_SI1145 si = Adafruit_SI1145();                      // SI1145 sensor
-Adafruit_BME280 bme;                                         // BME280 sensor
+Adafruit_SI1145 si1145 = Adafruit_SI1145();                  // SI1145 sensor
+Adafruit_BME280 bme280;                                      // BME280 sensor
 RTC_DS3231 rtc;                                              // DS3231 RTC module
 bool displayBME = true;                                      // Determines whether to display the info of BME280 on screen or not
 bool displaySI = false;                                      // Determines whether to display the info of SI1145 on screen or not
@@ -99,11 +99,11 @@ void screenSetup() {
 void checkSensors() {
   byte errors = 0;
   display.clearDisplay();
-  if (! bme.begin()) {
+  if (! bme280.begin()) {
     display.println("No valid BME280 found");
     errors += 1;
   }
-  if (! si.begin()) {
+  if (! si1145.begin()) {
     display.println("No valid SI1145 found");
     errors += 1;
   }
@@ -111,10 +111,11 @@ void checkSensors() {
     display.println("No RTC module found");
     errors += 1;
   }
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(CHIP_SELECT)) {
     display.println("SD card failed");
     errors += 1;
   } else {
+    File dataFile;
     dataFile = SD.open(FILE_NAME, FILE_WRITE);
     if (!dataFile) {
       dataFile.println("Could not open file");
@@ -133,7 +134,7 @@ void checkSensors() {
  */
 void writeHeadersOnFile() {
   File dataFile;
-  dataFile = SD.open(FILE_NAME, FILE_WRITE);
+  dataFile = SD.open("headers.csv", FILE_WRITE);
   if(dataFile) {
     dataFile.println("\"DateTime\",\"Temp\",\"Pressure\",\"Humidity\",\"Visible\",\"IR\",\"UV\"");
     dataFile.close();
@@ -144,15 +145,6 @@ void writeHeadersOnFile() {
     display.display();
     while(1);
   }
-}
-
-/**
- * Clears screen
- */
-void clearScreen() {
-  display.clearDisplay();
-  display.display();
-  display.setCursor(0, 0);
 }
 
 /**
@@ -169,100 +161,123 @@ void saveDateTime(File dataFile, bool displayEnabled) {
   dataFile.print(buffer);
 
   if (displayEnabled) {
-    display.clearDisplay();
-    sprintf(buffer, "%02lu/%02lu/%04lu ", now.day(), now.month(), now.year());
-    display.print(buffer);
-    sprintf(buffer, "%02lu:%02lu:%02lu", now.hour(), now.minute(), now.second());
-    display.println(buffer);
+    displayDateTime(now);
   }
 }
 
 /**
- * Gets a value of temperature, pressure and humidity
+ * Displays date and time on screen
+ * @param dateTime DateTime
+ */
+void displayDateTime(DateTime now) {
+  char buffer[13];
+  display.clearDisplay();
+  sprintf(buffer, "%02lu/%02lu/%04lu ", now.day(), now.month(), now.year());
+  display.print(buffer);
+  sprintf(buffer, "%02lu:%02lu:%02lu", now.hour(), now.minute(), now.second());
+  display.println(buffer);
+}
+
+void displayDateTime
+
+/**
+ * Writes value of temperature, pressure and humidity on SD card
  * @param dataFile       file on which to write
  * @param displayEnabled to display info on screen
  */
 void writeBME280Data(File dataFile, bool displayEnabled) {
   String dataString = "";
   float temp;
-  temp = bme.readTemperature();
-  Serial.print("Temperature = ");
-  Serial.print(temp);
-  Serial.print(" °C ");
+  temp = bme280.readTemperature();
   dataString += String(temp);
   dataString += ",";
 
   float pressure;
-  pressure = bme.readPressure() / 100.0F;
-  Serial.print("Pressure = ");
-  Serial.print(pressure);
-  Serial.print(" hPa ");
+  pressure = bme280.readPressure() / 100.0F;
   dataString += String(pressure);
   dataString += ",";
 
   float humidity;
-  humidity = bme.readHumidity();
-  Serial.print("Humidity = ");
-  Serial.print(humidity);
-  Serial.println(" %");
+  humidity = bme280.readHumidity();
   dataString += String(humidity);
   dataString += ",";
   dataFile.print(dataString);
 
   if (displayEnabled) {
-    display.print("Temp.: ");
-    display.print(temp);
-    display.print((char)247);
-    display.println("C");
-    display.print("Press.: ");
-    display.print(pressure);
-    display.println(" hPa");
-    display.print("Hum.: ");
-    display.print(humidity);
-    display.print(" %");
-    display.display();
-    display.setCursor(0, 0);
+    displayBME280Data(temp, pressure, humidity);
   }
 }
 
 /**
- * Gets a value of visible light, infrared and uv light
+ * Displays value of temperature, pressure and humidity on screen
+ * @param temp     temperature
+ * @param pressure atmospheric pressure
+ * @param humidity relative humidity
+ */
+void displayBME280Data(float temp, float pressure, float humidity) {
+  char buffer[18] = "";
+  sprintf(buffer, "Temp: %f %cC", temp, (char)247);
+  display.println(buffer);
+  sprintf(buffer, "Press: %f hPa", pressure);
+  display.println(buffer);
+  sprintf(buffer, "Hum: %f %", humidity);
+  display.println(buffer);
+  display.display();
+  display.setCursor(0, 0);
+}
+
+/**
+ * Writes value of visible light, infrared and uv light on SD card
  * @param dataFile       file on which to write
  * @param displayEnabled to display info on screen
  */
 void writeSI1145Data(File dataFile, bool displayEnabled) {
   String dataString = "";
   float visible;
-  visible = si.readVisible();
-  Serial.print("Vis: ");
-  Serial.print(visible);
+  visible = si1145.readVisible();
   dataString += String(visible);
   dataString += ",";
 
-  float ir;
-  ir = si.readIR();
-  Serial.print(" IR: ");
-  Serial.print(ir);
-  dataString += String(ir);
+  float irLight;
+  irLight = si1145.readIR();
+  dataString += String(irLight);
   dataString += ",";
 
-  float UVindex;
-  UVindex = si.readUV();
-  UVindex /= 100.0; // the index is multiplied by 100
-  Serial.print(" UV: ");
-  Serial.println(UVindex);
-  dataString += String(UVindex);
+  float uvIndex;
+  uvIndex = si1145.readUV();
+  uvIndex /= 100.0; // the index is multiplied by 100
+  dataString += String(uvIndex);
   dataFile.println(dataString);
 
   if (displayEnabled) {
-    display.print("Vis.: ");
-    display.println(visible);
-    display.print("IR: ");
-    display.println(ir);
-    display.print("UV: ");
-    display.print(UVindex);
-    display.display();
-    display.setCursor(0, 0);
+    displaySI1145Data(visible, irLight, uvIndex);
   }
+}
+
+/**
+ * Displays value of visible light, infrared light and uv light on screen
+ * @param visible visible light
+ * @param irLight infrared light
+ * @param uvIndex UV light index
+ */
+void displaySI1145Data(float visible, float irLight, float uvIndex) {
+  char buffer[18] = "";
+  sprintf(buffer, "Vis. light: %f", visible);
+  display.println(buffer);
+  sprintf(buffer, "IR light: %f", irLight);
+  display.println(buffer);
+  sprintf(buffer, "UV index: %f", uvIndex);
+  display.println(buffer);
+  display.display();
+  display.setCursor(0, 0);
+}
+
+/**
+ * Clears screen
+ */
+void clearScreen() {
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0, 0);
 }
 
